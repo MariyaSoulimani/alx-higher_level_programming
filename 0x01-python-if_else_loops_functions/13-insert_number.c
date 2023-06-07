8f0376cdbbea7519e89a8eec1b48aaba5adcd091
MariyaSoulimani <soulimanimaria@gmail.com>
2023-06-06 18:43:28 -0700

    13. Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,38 +9,25 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *curr, *tmp;
-    listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *node = *head, *new;
 
-    if (!new_node)
-        return (NULL);
-    new_node->n = number;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
 
-    if (!*head)
-    {
-        new_node->next = NULL;
-        *head = new_node;
-        return (new_node);
-    }
-    if ((*head)->n >= number)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
-    curr = *head;
-    while (curr->next)
-    {
-        if (curr->next->n >= number)
-        {
-            tmp = curr->next;
-            curr->next = new_node;
-            new_node->next = tmp;
-            return (new_node);
-        }
-        curr = curr->next;
-    }
-    curr->next = new_node;
-    new_node->next = NULL;
-    return (new_node);
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
 }
