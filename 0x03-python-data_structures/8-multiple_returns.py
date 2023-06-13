eb18989ead1bcb6f78ffd572c83e5a81cf767512
MariyaSoulimani <soulimanimaria@gmail.com>
2023-06-12 18:42:12 -0700

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,4 +2,5 @@
 def multiple_returns(sentence):
     if len(sentence) == None:
         return (0, None)
-    return (len(sentence), sentence[0])
+    else:
+        return (len(sentence), sentence[0])
