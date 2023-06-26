0aa53afffee064713125f9205903ad556587175c
MariyaSoulimani <soulimanimaria@gmail.com>
2023-06-26 16:25:42 -0700

    4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = [0] * list_length
-    index = 0
-    while index < list_length:
+    i = 0
+    while i < list_length:
         try:
             div = my_list_1[i] / my_list_2[i]
             div = 0
@@ -13,6 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-             new_list[index] = div
-        index += 1
+             new_list[i] = div
+        i += 1
     return (new_List)
