1f548efe0973159a11281af4a3047a5918034965
MariyaSoulimani <soulimanimaria@gmail.com>
2023-06-26 18:31:00 -0700

    10. CPython #2: PyFloatObject

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,5 +1,9 @@
 #include <Python.h>
-#include <stdio.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
  * print_python_list - Prints information about a Python list
  * @p: Pointer to the PyObject
